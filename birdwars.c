f9149f922a5a701ff886fe6df22746cdfec7bd54
alula <adamlula99@gmail.com>
2018-04-09 11:34:46 -0400

    Update birdwars.c

--- birdwars.c
+++ birdwars.c
@@ -196,7 +196,7 @@ void tim(){
 	}
 	if(seedshot > 0){seedshot--;}
 	if(seedshot < 0){seedshot = 0;}
-	seeds = (seedx != 0) + (seedx2 != 0) + (seedx3 != 0);
+	//seeds = (seedx != 0) + (seedx2 != 0) + (seedx3 != 0);
 }
 
 void out() //horrid beeping noise
@@ -311,49 +311,37 @@ void left(int *x,int *y){
 }
 void a(int x,int y){
     set_sprite_tile(1,10);
-	if(seedshot == 0 && seeds < 2) //was 3 changed to 2
-	{
-		switch(seeds)
-		{
-			case 0:
-				seed1:
-				out();
-				seedx = x + 12;
-				seedy = y +2;
-				seeds++;
-				set_sprite_tile(3,17);
-				move_sprite(3,seedx,seedy);
-				seedshot = 6;
-				break;
-			case 1:
-				seed2:
-				if(seedx2 != 0){goto seed1;}
-				seeds++;
-				out();
-				seedx2 = x + 12;
-				seedy2 = y +2;
-				set_sprite_tile(4,17);
-				move_sprite(4,seedx2,seedy2);
-				seedshot = 6;
-				break;
-			case 2:
-				seeds++;
-				out();
-				if(seedx3 != 0){goto seed2;}
-				seedx3 = x + 12;
-				seedy3 = y +2;
-				set_sprite_tile(5,17);
-				move_sprite(5,seedx3,seedy3);
-				seedshot = 6;
-				break;
-			default:
-				break;
-				
+	if(seedshot == 0){
+		if(seedx == 0){
+			out();
+			seedx = x + 12;
+			seedy = y + 2;
+			// seeds++;
+			set_sprite_tile(3,17);
+			move_sprite(3,seedx,seedy);
+			seedshot = 6;
+		}	
+		else if(seedx2 == 0){
+			// seeds++;
+			out();
+			seedx2 = x + 12;
+			seedy2 = y + 2;
+			seeds++;
+			set_sprite_tile(4,17);
+			move_sprite(4,seedx2,seedy2);
+			seedshot = 6;
 		}
-		
-			
+		else if(seedx3 == 0){
+			// seeds++;
+			out();
+			seedx3 = x + 12;
+			seedy3 = y +2;
+			set_sprite_tile(5,17);
+			move_sprite(5,seedx3,seedy3);
+			seedshot = 6;
+		}	
 	}
-		// printf("%d, %d, %d, %d, %d\n", seedx, seedx2, seedx3, seeds, seedshot);
+		//printf("%d, %d, %d, %d, %d\n", seedx, seedx2, seedx3, seeds, seedshot);
 }
 void a_rel(){
 	set_sprite_tile(1,2);
